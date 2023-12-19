800ff1985f66e95842b5f552b2e6d3b7f1266a01
Emerson Willian <emersonwlw@gmail.com>
2023-12-18 23:14:11 -0300

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/container"
+
+
+export default function Dashboard() {
+    return(
+        <Container>
+            <h1>Pagina Dashboard</h1>
+        </Container>
+    )
+}
